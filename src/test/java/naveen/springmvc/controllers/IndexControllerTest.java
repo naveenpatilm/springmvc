89f75a2c26abc00bfd4d7c9fb84116289c843e83
Naveen Patil <naveenpatilm@gmail.com>
2016-08-21 23:11:50 +0530

    Add spring mvc test for index controller

---
+++ src/test/java/naveen/springmvc/controllers/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package naveen.springmvc.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class IndexControllerTest {
+	private MockMvc mockMvc;
+	private IndexController indexController;
+
+	@Before
+	public void setUp() {
+		indexController = new IndexController();
+		mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+	}
+
+	@Test
+	public void testIndex() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+	}
+
+}
